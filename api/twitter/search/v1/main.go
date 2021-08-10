29699455cc073092889a9dab9ad5425ffaefabf6
Willberg <hszhongtao@163.com>
2021-08-10 23:24:28 +0800

    twitter api

---
+++ api/twitter/search/v1/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/go-yaml/yaml"
+	"github.com/tidwall/pretty"
+	"io/ioutil"
+	"log"
+	"net/http"
+	url2 "net/url"
+)
+
+type env struct {
+	Ak  string `yaml:"apiKey"`
+	Ask string `yaml:"apiSecretKey"`
+	Bt  string `yaml:"bearerToken"`
+	At  string `yaml:"accessToken"`
+	Ats string `yaml:"accessTokenSecret"`
+}
+
+var fp = flag.String("p", "/home/john/mine/workplace/go/ember/api/twitter/env/keys.yml", "配置文件路径")
+
+func main() {
+	flag.Parse()
+
+	e := env{}
+	data, err := ioutil.ReadFile(*fp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(data, &e)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	encondeStr := url2.QueryEscape("epidemic situation")
+	url := "https://api.twitter.com/2/tweets/search/recent?query=" + encondeStr
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bt := "Bearer " + e.Bt
+	req.Header.Add("Authorization", bt)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s\n", pretty.Pretty(body))
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module ember
 go 1.15
 
 require (
+	github.com/dghubble/go-twitter v0.0.0-20210609183100-2fdbf421508e // indirect
+	github.com/g8rswimmer/go-twitter v1.1.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/golang/glog v0.0.0-20210429001901-424d2337a529
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/tidwall/pretty v1.2.0
 	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
 	gopkg.in/xmlpath.v2 v2.0.0-20150820204837-860cbeca3ebc
 )
