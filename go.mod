20d3b511e24ff5fbfbcad85e6c919be6bbedc460
Willberg <hszhongtao@163.com>
2022-10-04 11:01:39 +0800

    1. benchmark测试 2. assert测试

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/golang/glog v0.0.0-20210429001901-424d2337a529
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/pretty v1.2.0
 	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- test/int_slice_test.go
+++ test/int_slice_test.go
@@ -1,13 +1,21 @@
 package test
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"sort"
 	"testing"
 )
 
-func TestIntSlice(t *testing.T) {
+func TestSortSearch(t *testing.T) {
+	a := assert.New(t)
 	pos := sort.IntSlice{1, 2, 4, 5, 6}
 	k := pos.Search(3)
-	fmt.Println(k)
+	a.Equal(k, 2)
+}
+
+func BenchmarkSortSearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pos := sort.IntSlice{1, 2, 4, 5, 6}
+		pos.Search(3)
+	}
 }
